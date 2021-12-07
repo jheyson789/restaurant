99e5575f74d9036326acd752d8f3690137651f52
Jheyson Zurita <jheysonslash789@gmail.com>
2021-12-07 00:12:28 -0400

    Services (#2)
    * container style
    
    * fix class container
    
    * section service menu info
    
    * change to box shadow
    
    * setting font size
    
    * text
    
    Co-authored-by: jheyson zurita soria <jheysoria20@gmai.com>

---
+++ src/app/main/home/pages/services-menu/services-menu.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ServicesMenuComponent } from './services-menu.component';
+
+describe('ServicesMenuComponent', () => {
+  let component: ServicesMenuComponent;
+  let fixture: ComponentFixture<ServicesMenuComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ServicesMenuComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ServicesMenuComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
