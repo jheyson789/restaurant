46f5da6f976313c9276af6ced1baa0aba6249096
jheyson zurita soria <jheysoria20@gmai.com>
2021-12-08 12:10:55 -0400

    function search type item the menu

--- src/app/main/home/pages/main-menu/main-menu.component.ts
+++ src/app/main/home/pages/main-menu/main-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../data/menu.data.json';
+import * as dataItems from '../data/menu-items.data.json';
+
 
 @Component({
   selector: 'app-main-menu',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-menu.component.css']
 })
 export class MainMenuComponent implements OnInit {
+  private menuData = data
+  public itemsMenuData = dataItems
+
+  public isActived = ''
+
+  public itemsMenu: any[] = []
 
   constructor() { }
 
   ngOnInit(): void {
+    this.searchTypeMenu('starters')
   }
+
+  searchTypeMenu(itemType: string) {
+    this.isActived = itemType
+    this.itemsMenu = this.menuData.data.find(item => item.typeMenu === itemType )?.data!
+  } 
 
 }
