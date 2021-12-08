4b3190176dc36f2622a78158efa3851f0d58902f
jheyson zurita soria <jheysoria20@gmai.com>
2021-12-08 12:04:07 -0400

    import compenent module

--- src/app/main/home/pages/pages.module.ts
+++ src/app/main/home/pages/pages.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ServicesMenuComponent } from './services-menu/services-menu.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
+import { ComponentsModule } from './components/components.module';
 
 
 
@@ -11,7 +12,8 @@ import { MainMenuComponent } from './main-menu/main-menu.component';
     MainMenuComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentsModule
   ],
   exports: [
     ServicesMenuComponent,
